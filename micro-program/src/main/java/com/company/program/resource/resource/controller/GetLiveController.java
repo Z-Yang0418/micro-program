e795a5d5622e50608df01f594ebf64aaa94d7462
Zhao <619429468@qq.com>
2019-01-20 04:12:16 +0800

    指定频率直播列表

--- micro-program/src/main/java/com/company/program/resource/resource/controller/GetLiveController.java
+++ micro-program/src/main/java/com/company/program/resource/resource/controller/GetLiveController.java
@@ -4,8 +4,6 @@ import com.company.program.resource.resource.service.GetLiveService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
